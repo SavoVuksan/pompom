7aec342583acc8696476c73570ffff8945a55e7d
Savo Vuksan <savovuksan@gmail.com>
2024-04-10 14:15:16 +0200

    Added PrimeNg

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,9 +1,12 @@
 import { Component, HostBinding } from '@angular/core';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'timer',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule
+  ],
   templateUrl: './timer.component.html',
   styleUrl: './timer.component.scss',
   host:{'class': 'p-4 m-4 flex flex-col rounded border gap-4'}
