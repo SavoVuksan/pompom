d4040c8d8f179a79b876130f021d9db1e721181b
Savo Vuksan <savovuksan@gmail.com>
2024-04-10 14:43:35 +0200

    WIP Timer Component

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,15 +1,16 @@
 import { Component, HostBinding } from '@angular/core';
 import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
 
 @Component({
   selector: 'timer',
   standalone: true,
   imports: [
-    ButtonModule
+    ButtonModule, CardModule,
   ],
   templateUrl: './timer.component.html',
   styleUrl: './timer.component.scss',
-  host:{'class': 'p-4 m-4 flex flex-col rounded border gap-4'}
+  host:{'class': 'p-4 m-4'}
 })
 export class TimerComponent {
 
