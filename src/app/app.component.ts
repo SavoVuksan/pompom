45f1b81e786b958b76b7037ddfd9811c072fb7ed
Savo Vuksan <savo.vuksan@x-tention.at>
2024-05-06 13:51:18 +0200

    added basic header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TimerComponent } from './features/timer/components/timer/timer.component';
 import { PomodoroComponent } from './features/timer/components/pomodoro/pomodoro.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TimerComponent, PomodoroComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TimerComponent,
+    PomodoroComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
