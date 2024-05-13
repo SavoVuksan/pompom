08af70f301109a92666ea58135a6e850e2c7834d
Savo Vuksan <savovuksan@gmail.com>
2024-05-13 17:16:50 +0200

    started implementing pomodoro settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,32 @@ import { HeaderComponent } from './components/header/header.component';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { AppStore } from './stores/app.store';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { PomodoroStore } from './stores/pomodoro.store';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
     RouterOutlet,
     TimerComponent,
     HeaderComponent,
     TimerComponent,
     DialogModule,
     ButtonModule,
+    InputNumberModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   appStore = inject(AppStore);
+  pomodoroStore = inject(PomodoroStore);
   title = 'Pom Pom';
+  focusDuration: number = 25;
 
   onVisibilityChange(visible: boolean) {
     if (!visible) {
